4145486bf5e16b44f040f3e5a56ee9c4e5de456b
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-09-13 17:43:53 +0300

    +

--- src/pkg/democrypt/rsa_test.go
+++ src/pkg/democrypt/rsa_test.go
@@ -8,68 +8,119 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func getRsa(Type int) (*RsaCrypt, error) {
+	cwd, _ := os.Getwd()
+	rsaPath := filepath.Join(cwd, "..", "..", "..", "cert", "rsa")
+	return NewRSACrypt(filepath.Join(rsaPath, "cert.pem"), filepath.Join(rsaPath, "key.pem"), Type)
+}
+
 func TestNewRSACrypt(t *testing.T) {
 	a := assert.New(t)
 
-	cwd, _ := os.Getwd()
-	rsaPath := filepath.Join(cwd, "..", "..", "..", "cert", "rsa")
-	_, err := NewRSACrypt(filepath.Join(rsaPath, "cert.pem"), filepath.Join(rsaPath, "key.pem"), RsaOaep)
+	_, err := getRsa(RsaOaep)
 	a.NoError(err)
 }
 
-// func TestRSAEncryptDecrypt(t *testing.T) {
-// 	a := assert.New(t)
+func TestRSAEncryptDecrypt_Oaep(t *testing.T) {
+	a := assert.New(t)
 
-// 	cwd, _ := os.Getwd()
-// 	rsaPath := filepath.Join(cwd, "..", "..", "..", "cert", "rsa")
-// 	rsacrypt, err := NewRSACrypt(filepath.Join(rsaPath, "cert.pem"), filepath.Join(rsaPath, "key.pem"))
-// 	a.NoError(err)
+	rsacrypt, err := getRsa(RsaOaep)
+	a.NoError(err)
 
-// 	encrypted, err := rsacrypt.Encrypt("test")
-// 	a.NoError(err)
+	for _, item := range data {
+		encrypted, err := rsacrypt.Encrypt(item)
+		a.NoError(err)
 
-// 	decrypted, err := rsacrypt.Decrypt(encrypted)
-// 	a.NoError(err)
-// 	a.Equal(decrypted, "test")
-// }
+		decrypted, err := rsacrypt.Decrypt(encrypted)
+		a.NoError(err)
+		a.Equal(decrypted, item)
+	}
+}
 
-// func TestRSADecrypt_FromCSharp(t *testing.T) {
-// 	t.Skip("no data available")
-// 	a := assert.New(t)
+func TestRSAEncryptDecrypt_Pkcs1v15(t *testing.T) {
+	a := assert.New(t)
 
-// 	cwd, _ := os.Getwd()
-// 	rsaPath := filepath.Join(cwd, "..", "..", "..", "cert", "rsa")
-// 	rsacrypt, err := NewRSACrypt(filepath.Join(rsaPath, "cert.pem"), filepath.Join(rsaPath, "key.pem"))
-// 	a.NoError(err)
+	rsacrypt, err := getRsa(RsaPkcs1V15)
+	a.NoError(err)
 
-// 	decrypted, err := rsacrypt.Decrypt("")
-// 	a.NoError(err)
-// 	a.Equal(decrypted, data)
-// }
+	for _, item := range data {
+		encrypted, err := rsacrypt.Encrypt(item)
+		a.NoError(err)
 
-// func TestRSADecrypt_FromJs(t *testing.T) {
-// 	a := assert.New(t)
+		decrypted, err := rsacrypt.Decrypt(encrypted)
+		a.NoError(err)
+		a.Equal(decrypted, item)
+	}
+}
 
-// 	cwd, _ := os.Getwd()
-// 	rsaPath := filepath.Join(cwd, "..", "..", "..", "cert", "rsa")
-// 	rsacrypt, err := NewRSACrypt(filepath.Join(rsaPath, "cert.pem"), filepath.Join(rsaPath, "key.pem"))
-// 	a.NoError(err)
+func TestRSADecrypt_FromCSharp_Oaep(t *testing.T) {
+	t.Skip("no data available")
+	a := assert.New(t)
 
-// 	decrypted, err := rsacrypt.Decrypt("HDIXtNnbJjaxfCKFEmecq2LL32nDmzn+p5FsZZZYtDUKrf6VPAFI2iNxXVlYcJjoJ41z70zRQg1+Y28bfAvlplqCRV1REXDsVMWWTq02g/oJTd2bwB/0ly3lb6tEFbCK6JQ9WBLU4LcX6qgZsJZWJIWhBimNFA+wEIwFxZcR5csqg3/YDdnVafD8ydywsQPp7pQ3FbFseHt15nNJ9Hg+jbbjb3n9TAeJ0J8BJ0q7ocd9tbC46VaviaAI2tyF9sarMdR2AdywwrLzCaJk1a1UibCCvVH58H+08utb/RjvWj/VU1UfDdvmeoinxQKLVbzZDfkGmctDE1AcOW4CJ0+Gzw==")
-// 	a.NoError(err)
-// 	a.Equal(decrypted, data)
-// }
+	rsacrypt, err := getRsa(RsaOaep)
+	a.NoError(err)
 
-// func TestRSADecrypt_FromPy(t *testing.T) {
-// 	t.Skip("no data available")
-// 	a := assert.New(t)
+	decrypted, err := rsacrypt.Decrypt(CS_RSA_OAEP)
+	a.NoError(err)
+	a.Equal(decrypted, data[0])
+}
 
-// 	cwd, _ := os.Getwd()
-// 	rsaPath := filepath.Join(cwd, "..", "..", "..", "cert", "rsa")
-// 	rsacrypt, err := NewRSACrypt(filepath.Join(rsaPath, "cert.pem"), filepath.Join(rsaPath, "key.pem"))
-// 	a.NoError(err)
+func TestRSADecrypt_FromCSharp_Pkcs1v15(t *testing.T) {
+	t.Skip("no data available")
+	a := assert.New(t)
 
-// 	decrypted, err := rsacrypt.Decrypt("")
-// 	a.NoError(err)
-// 	a.Equal(decrypted, data)
-// }
+	rsacrypt, err := getRsa(RsaPkcs1V15)
+	a.NoError(err)
+
+	decrypted, err := rsacrypt.Decrypt(CS_RSA_OAEP)
+	a.NoError(err)
+	a.Equal(decrypted, data[0])
+}
+
+func TestRSADecrypt_FromJs_Oaep(t *testing.T) {
+	t.Skip("no data available")
+	a := assert.New(t)
+
+	rsacrypt, err := getRsa(RsaOaep)
+	a.NoError(err)
+
+	decrypted, err := rsacrypt.Decrypt(JS_RSA_OAEP)
+	a.NoError(err)
+	a.Equal(decrypted, data[0])
+}
+
+func TestRSADecrypt_FromJs_Pkcs1v15(t *testing.T) {
+	t.Skip("no data available")
+	a := assert.New(t)
+
+	rsacrypt, err := getRsa(RsaPkcs1V15)
+	a.NoError(err)
+
+	decrypted, err := rsacrypt.Decrypt(JS_RSA_OAEP)
+	a.NoError(err)
+	a.Equal(decrypted, data[0])
+}
+
+func TestRSADecrypt_FromPy_Oaep(t *testing.T) {
+	t.Skip("no data available")
+	a := assert.New(t)
+
+	rsacrypt, err := getRsa(RsaOaep)
+	a.NoError(err)
+
+	decrypted, err := rsacrypt.Decrypt(PY_RSA_OAEP)
+	a.NoError(err)
+	a.Equal(decrypted, data[0])
+}
+
+func TestRSADecrypt_FromPy_Pkcs1v15(t *testing.T) {
+	t.Skip("no data available")
+	a := assert.New(t)
+
+	rsacrypt, err := getRsa(RsaPkcs1V15)
+	a.NoError(err)
+
+	decrypted, err := rsacrypt.Decrypt(PY_RSA_OAEP)
+	a.NoError(err)
+	a.Equal(decrypted, data[0])
+}
